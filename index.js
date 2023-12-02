8b9c64ca6371e1f51bcc6e4466b32dbce70aac6e
judamar <juancho.o.mart@gmail.com>
2023-12-01 23:16:24 -0500

    using express

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const http = require('node:http') // protocolo http
+const express = require('express')
+const app = express()
 
-const desiredPort = process.env.PORT ?? 3000
+const PORT = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-  console.log('request received: ', req.url)
-  res.end('Hola mundo')
+app.get('/', (req, res) => {
+  res.status(200).send('<h1>Ferreteria Construmanta P</h1>')
 })
 
-server.listen(desiredPort, () => {
-  console.log(`server listening on port http://localhost:${desiredPort}`)
+app.get('/acerca', (req, res) => {
+  res.status(200).send('<h1>Sobre nosotros</h1>')
+})
+
+app.get('/contacto', (req, res) => {
+  res.status(200).send('<h1>Contactanos</h1>')
+})
+
+app.get('/catalogo', (req, res) => {
+  res.status(200).send('<h1>Catálogo</h1>')
+})
+
+app.get('/login', (req, res) => {
+  res.status(200).send('<h1>Iniciar Sesión</h1>')
+})
+
+app.use('*', (req, res) => {
+  res.status(404).send('<h1>404 not found, go back to home</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
