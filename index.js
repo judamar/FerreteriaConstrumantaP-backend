d92b38da382254b4bce209256f3b95cccb3ff1d8
judamar <juancho.o.mart@gmail.com>
2023-12-02 12:55:32 -0500

    coso

--- index.js
+++ index.js
@@ -3,31 +3,7 @@ const express = require('express')
 const PORT = process.env.PORT ?? 3000
 
 const app = express()
-app.disable('x-powered-by')
-
-app.use((req, res, next) => {
-  if (req.method !== 'POST') return next()
-})
-
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Ferreteria Construmanta P</h1>')
-})
-
-app.get('/acerca', (req, res) => {
-  res.status(200).send('<h1>Sobre nosotros</h1>')
-})
-
-app.get('/contacto', (req, res) => {
-  res.status(200).send('<h1>Contactanos</h1>')
-})
-
-app.get('/catalogo', (req, res) => {
-  res.status(200).send('<h1>Catálogo</h1>')
-})
-
-app.get('/login', (req, res) => {
-  res.status(200).send('<h1>Iniciar Sesión</h1>')
-})
+app.disable('x-powered-by') // deshabilita la cabecera X-Powered-By
 
 app.use('*', (req, res) => {
   res.status(404).send('<h1>404 not found, go back to home</h1>')
