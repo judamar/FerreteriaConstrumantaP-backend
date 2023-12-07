68853af8d3fd70c93f0ac58a729112cdefb760b9
judamar <juancho.o.mart@gmail.com>
2023-12-06 23:43:22 -0500

    db config improved

--- src/database/db.js
+++ src/database/db.js
@@ -3,13 +3,11 @@ import 'dotenv/config'
 import fs from 'fs'
 
 const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_DATABASE,
-  ssl: {
-    ca: fs.readFileSync('cacert-2023-08-22.pem')
-  }
+  ssl: fs.readFileSync(process.env.SSL_KEY)
 })
 
 export default pool
