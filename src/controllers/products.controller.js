c50f0a47536bf12005eef72a59551c9106b39d30
judamar <juancho.o.mart@gmail.com>
2023-12-22 12:28:51 -0500

    finishing products (not tested)

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -3,31 +3,51 @@ import Product from '../models/product.model.js'
 const getAllProducts = async (req, res) => {
   try {
     const products = await Product.getAllProducts()
-    res.status(200).json({ products })
+    res.status(200).json({ status: 'OK', products })
   } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los productos' })
+    res.status(500).json({ message: error.message })
   }
 }
 
 const getOneProduct = async (req, res) => {
   try {
-    const product = await Product.getProductById(req.params.productId)
-    res.status(200).json({ product })
+    const product = await Product.getProductById(req.params.id)
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' })
+    }
+    res.status(200).json({ status: 'OK', product })
   } catch (error) {
-    res.status(500).json({ error: 'Error al obtener el producto' })
+    res.status(500).json({ message: error.message })
   }
 }
 
 const createNewProduct = (req, res) => {
-  res.send(`Create new product ${req.params.productId}`)
+  try {
+    const product = req.body.product
+    const result = Product.createProduct(product)
+    res.status(201).json({ status: 'OK', product: result })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 const updateOneProduct = (req, res) => {
-  res.send(`Update product ${req.params.productId}`)
+  try {
+    const product = req.body.product
+    const result = Product.updateProduct(req.params.id, product)
+    res.status(201).json({ status: 'OK', product: result })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 const deleteOneProduct = (req, res) => {
-  res.send(`Delete product ${req.params.productId}`)
+  try {
+    const result = Product.deleteProduct(req.params.id)
+    res.status(200).json({ status: 'OK', product: result })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
 export default {
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -22,6 +22,42 @@ class Product {
         })
     })
   }
+
+  static insertProduct (product) {
+    return new Promise((resolve, reject) => {
+      pool.query('INSERT INTO producto (nombre_producto, clave_producto, url_imagen, marca, descripcion, precio, cantidad, categoria_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [product.nombre_producto, product.clave_producto, product.url_imagen, product.marca, product.descripcion, product.precio, product.cantidad, product.categoria_id])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
+
+  static updateProduct (id, product) {
+    return new Promise((resolve, reject) => {
+      pool.query('UPDATE producto SET nombre_producto = ?, clave_producto = ?, url_imagen = ?, marca = ?, descripcion = ?, precio = ?, cantidad = ?, categoria_id = ? WHERE id = ?', [product.nombre_producto, product.clave_producto, product.url_imagen, product.marca, product.descripcion, product.precio, product.cantidad, product.categoria_id, id])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
+
+  static deleteProduct (id) {
+    return new Promise((resolve, reject) => {
+      pool.query('DELETE FROM producto WHERE id = ?', [id])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
 }
 
 export default Product
