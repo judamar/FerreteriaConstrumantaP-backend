7d9af83357fb583d38c0a0bfb3eba08979e7328e
judamar <juancho.o.mart@gmail.com>
2023-12-23 12:56:23 -0500

    password

---
+++ src/utils/password.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt'
+
+const hashPassword = (password) => {
+  if (password.length < 8) {
+    throw new Error('Password must be 8 characters or longer.')
+  }
+  return bcrypt.hash(password, 10)
+}
+
+const comparePassword = (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword)
+}
+
+export default { hashPassword, comparePassword }
