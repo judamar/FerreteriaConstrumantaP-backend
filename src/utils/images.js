8b8406b279b2dffc9565734e914af169c4c62d40
judamar <juancho.o.mart@gmail.com>
2024-01-07 18:58:46 -0500

    imageToURL

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,3 +1,25 @@
 import 'dotenv/config'
+import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
 
-const CLIENT_ID = process.env.IMAGUR_CLIENT_ID
+const CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
+export const imageToURL = async (filePath) => {
+  try {
+    const extension = path.extname(filePath).slice(1)
+    const imageData = fs.readFileSync(filePath)
+
+    const response = await axios.post('https://api.imgur.com/3/image', imageData, {
+      headers: {
+        Authorization: `Client-ID ${CLIENT_ID}`,
+        'Content-Type': `image/${extension}`
+      }
+    })
+
+    return response.data.data.link
+  } catch (error) {
+    console.error('Error', error)
+    return null
+  }
+}
