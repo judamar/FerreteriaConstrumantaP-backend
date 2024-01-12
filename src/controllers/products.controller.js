533b4c9a54e3671014c83b2ebb33966bd01c0ce1
judamar <juancho.o.mart@gmail.com>
2024-01-12 17:37:54 -0500

    push to continue later

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,23 +1,42 @@
 import Product from '../models/product.model.js'
 
-const getAllProducts = async (req, res) => {
+const getAll = async (req, res) => {
   try {
-    const products = await Product.getAllProducts()
-    res.status(200).json({ status: 'OK', products })
+    const products = await Product.getAll()
+    if (!products) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'Products not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', products })
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
-const getOneProduct = async (req, res) => {
+const getById = async (req, res) => {
   try {
-    const product = await Product.getProductById(req.params.id)
+    const product = await Product.getById(req.params.id)
     if (!product) {
-      return res.status(404).json({ message: 'Product not found' })
+      res.status(404).json({ status: 'NOT_FOUND', error: 'Product not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', product })
     }
-    res.status(200).json({ status: 'OK', product })
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ status: 'ERROR', error: error.message })
+  }
+}
+
+const getByName = async (req, res) => {
+  const name = req.params.name
+  try {
+    const product = await Product.getByName(name)
+    if (!product) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'Product not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', product })
+    }
+  } catch (error) {
+    res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
@@ -25,9 +44,14 @@ const createNewProduct = (req, res) => {
   try {
     const product = req.body.product
     const result = Product.insertProduct(product)
-    res.status(201).json({ status: 'OK', product: result })
+    if (!result) {
+      res.status(400).json({ status: 'ERROR', error: 'Product not created' })
+    } else {
+      res.status(201).json({ status: 'RESOURCE_CREATED', product: result })
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ status: 'ERROR', error: error.message })
+
   }
 }
 
@@ -35,18 +59,26 @@ const updateOneProduct = (req, res) => {
   try {
     const product = req.body.product
     const result = Product.updateProduct(req.params.id, product)
-    res.status(201).json({ status: 'OK', product: result })
+    if (!result) {
+      res.status(500).json({ status: 'ERROR', error: 'Product not updated' })
+    } else {
+      res.status(200).json({ status: 'RESOURCE_UPDATED', product: result })
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
 const deleteOneProduct = (req, res) => {
   try {
     const result = Product.deleteProduct(req.params.id)
-    res.status(200).json({ status: 'OK', product: result })
+    if (!result) {
+      res.status(500).json({ status: 'ERROR', error: 'Product not deleted' })
+    } else {
+      res.status(200).json({ status: 'RESOURCE_DELETED', product: result })
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,7 +1,7 @@
 import pool from '../database/db.js'
 
 class Product {
-  static getAllProducts () {
+  static getAll () {
     return new Promise((resolve, reject) => {
       pool.query('SELECT p.id, p.nombre_producto, p.clave_producto, p.url_imagen, p.marca, p.descripcion, p.precio, p.cantidad, c.categoria AS categoria FROM productos p JOIN categorias c ON p.categorias_id = c.id')
         .then(([rows, fields]) => {
@@ -11,7 +11,7 @@ class Product {
     })
   }
 
-  static getProductById (id) {
+  static getById (id) {
     return new Promise((resolve, reject) => {
       pool.query('SELECT * FROM productos WHERE id = ?', [id])
         .then(([rows, fields]) => {
@@ -23,7 +23,43 @@ class Product {
     })
   }
 
-  static insertProduct (product) {
+  static getByName (name) {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM productos WHERE nombre_producto = ?', [name])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
+
+  static getByCategory (category) {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM productos WHERE categoria = ?', [category])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
+
+  static getByKey (key) {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM productos WHERE clave_producto = ?', [key])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(error => {
+          reject(error)
+        })
+    })
+  }
+
+  static create (product) {
     return new Promise((resolve, reject) => {
       pool.query('INSERT INTO productos (nombre_producto, clave_producto, url_imagen, marca, descripcion, precio, cantidad, categoria_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [product.nombre_producto, product.clave_producto, product.url_imagen, product.marca, product.descripcion, product.precio, product.cantidad, product.categoria_id])
         .then(([rows, fields]) => {
@@ -35,7 +71,7 @@ class Product {
     })
   }
 
-  static updateProduct (id, product) {
+  static update (id, product) {
     return new Promise((resolve, reject) => {
       pool.query('UPDATE productos SET nombre_producto = ?, clave_producto = ?, url_imagen = ?, marca = ?, descripcion = ?, precio = ?, cantidad = ?, categoria_id = ? WHERE id = ?', [product.nombre_producto, product.clave_producto, product.url_imagen, product.marca, product.descripcion, product.precio, product.cantidad, product.categoria_id, id])
         .then(([rows, fields]) => {
@@ -47,7 +83,7 @@ class Product {
     })
   }
 
-  static deleteProduct (id) {
+  static remove (id) {
     return new Promise((resolve, reject) => {
       pool.query('DELETE FROM productos WHERE id = ?', [id])
         .then(([rows, fields]) => {
