c7ac9dafb85f49b47a82f3a8f2dd1fa003a173f4
judamar <juancho.o.mart@gmail.com>
2024-01-11 23:11:43 -0500

    improving category controller

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -3,54 +3,70 @@ import Category from '../models/category.model.js'
 const getAll = async (req, res) => {
   try {
     const categories = await Category.getAll()
-    res.status(200).json({ status: 'OK', categories })
+    if (!categories) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'Categories not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', categories })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
 const getById = async (req, res) => {
+  const id = req.params.id
   try {
-    const category = await Category.getById(req.params.id)
-    res.status(200).json({ status: 'OK', category })
+    const category = await Category.getById(id)
+    if (!category) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'Category not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', category })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
 const getByName = async (req, res) => {
+  const name = req.params.name
   try {
-    const category = await Category.getByName(req.params.name)
-    res.status(200).json({ status: 'OK', category })
+    const category = await Category.getByName(name)
+    if (!category) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'Category not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', category })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
 const insert = async (req, res) => {
+  const category = req.body.category
   try {
-    const category = req.body.categoria
     const result = await Category.createCategory(category)
-    res.status(201).json({ status: 'OK', category: result })
+    res.status(201).json({ status: 'RESOURCE_CREATED', category: result })
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
 const update = async (req, res) => {
+  const id = req.params.id
+  const category = req.body.category
   try {
-    const category = req.body.categoria
-    const result = await Category.updateCategory(req.params.id, category)
-    res.status(201).json({ status: 'OK', category: result })
+    const result = await Category.updateCategory(id, category)
+    res.status(201).json({ status: 'RESOURCE_UPDATED', category: result })
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
 }
 
 const remove = async (req, res) => {
+  const id = req.params.id
   try {
-    const result = await Category.deleteCategory(req.params.id)
-    res.status(200).json({ status: 'OK', category: result })
+    const result = await Category.deleteCategory(id)
+    res.status(200).json({ status: 'RESOURCE_DELETED', category: result })
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
