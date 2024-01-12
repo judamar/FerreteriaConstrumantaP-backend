6b05dc987866cedf6ce475f1d5493f8915da0edb
judamar <juancho.o.mart@gmail.com>
2024-01-11 23:11:14 -0500

    improving user controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,7 +3,11 @@ import User from '../models/user.model.js'
 const getAll = async (req, res) => {
   try {
     const users = await User.getAll()
-    res.status(200).json({ status: 'OK', users })
+    if (!users) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'No users found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', users })
+    }
   } catch (error) {
     res.status(404).json({ status: 'ERROR', error: error.message })
   }
@@ -13,7 +17,11 @@ const getById = async (req, res) => {
   const id = req.params.id
   try {
     const user = await User.getById(id)
-    res.status(200).json({ status: 'OK', user })
+    if (!user) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'User not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', user })
+    }
   } catch (error) {
     res.status(404).json({ status: 'ERROR', error: error.message })
   }
@@ -23,7 +31,11 @@ const getByCedula = async (req, res) => {
   const cedula = req.params.cedula
   try {
     const user = await User.getByCedula(cedula)
-    res.status(200).json({ status: 'OK', user })
+    if (!user) {
+      res.status(404).json({ status: 'NOT_FOUND', error: 'User not found' })
+    } else {
+      res.status(200).json({ status: 'SUCCESS', user })
+    }
   } catch (error) {
     res.status(404).json({ status: 'ERROR', error: error.message })
   }
@@ -33,7 +45,11 @@ const insert = async (req, res) => {
   const user = req.body.user
   try {
     const result = await User.create(user)
-    res.status(201).json({ status: 'OK', user: result })
+    if (!result) {
+      res.status(500).json({ status: 'ERROR', error: 'User not created' })
+    } else {
+      res.status(201).json({ status: 'RESOURCE_CREATED', user: result })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
@@ -44,7 +60,11 @@ const updateById = async (req, res) => {
   const user = req.body.user
   try {
     const result = await User.updateById(id, user)
-    res.status(201).json({ status: 'OK', user: result })
+    if (!result) {
+      res.status(500).json({ status: 'ERROR', error: 'User not updated' })
+    } else {
+      res.status(201).json({ status: 'RESOURCE_UPDATED', user: result })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
@@ -55,7 +75,11 @@ const updateByCedula = async (req, res) => {
   const user = req.body.user
   try {
     const result = await User.updateByCedula(cedula, user)
-    res.status(201).json({ status: 'OK', user: result })
+    if (!result) {
+      res.status(500).json({ status: 'ERROR', error: 'User not updated' })
+    } else {
+      res.status(201).json({ status: 'RESOURCE_UPDATED', user: result })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
@@ -65,7 +89,11 @@ const remove = async (req, res) => {
   const id = req.params.id
   try {
     const result = await User.delete(id)
-    res.status(200).json({ status: 'OK', user: result })
+    if (!result) {
+      res.status(500).json({ status: 'ERROR', error: 'User not deleted' })
+    } else {
+      res.status(200).json({ status: 'RESOURCE_DELETED', user: result })
+    }
   } catch (error) {
     res.status(500).json({ status: 'ERROR', error: error.message })
   }
