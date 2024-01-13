abdc979bf0046cec5be714edf35c2ce5cbdedc6b
judamar <juancho.o.mart@gmail.com>
2024-01-12 22:39:11 -0500

    using handles and ternaries

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,15 +1,12 @@
 import User from '../models/user.model.js'
+import { handleSuccess, handleNotFound, handleServerError, handleBadRequest } from '../utils/handles.js'
 
 const getAll = async (req, res) => {
   try {
     const users = await User.getAll()
-    if (!users) {
-      res.status(404).json({ status: 'NOT_FOUND', error: 'No users found' })
-    } else {
-      res.status(200).json({ status: 'SUCCESS', users })
-    }
+    users ? handleSuccess(res, users) : handleNotFound(res, 'Users not found.')
   } catch (error) {
-    res.status(404).json({ status: 'ERROR', error: error.message })
+    handleBadRequest(res, error.message)
   }
 }
 
@@ -17,13 +14,9 @@ const getById = async (req, res) => {
   const id = req.params.id
   try {
     const user = await User.getById(id)
-    if (!user) {
-      res.status(404).json({ status: 'NOT_FOUND', error: 'User not found' })
-    } else {
-      res.status(200).json({ status: 'SUCCESS', user })
-    }
+    user ? handleSuccess(res, user) : handleNotFound(res, 'User not found.')
   } catch (error) {
-    res.status(404).json({ status: 'ERROR', error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
@@ -31,11 +24,7 @@ const getByCedula = async (req, res) => {
   const cedula = req.params.cedula
   try {
     const user = await User.getByCedula(cedula)
-    if (!user) {
-      res.status(404).json({ status: 'NOT_FOUND', error: 'User not found' })
-    } else {
-      res.status(200).json({ status: 'SUCCESS', user })
-    }
+    user ? handleSuccess(res, user) : handleNotFound(res, 'User not found.')
   } catch (error) {
     res.status(404).json({ status: 'ERROR', error: error.message })
   }
@@ -45,13 +34,9 @@ const insert = async (req, res) => {
   const user = req.body.user
   try {
     const result = await User.create(user)
-    if (!result) {
-      res.status(500).json({ status: 'ERROR', error: 'User not created' })
-    } else {
-      res.status(201).json({ status: 'RESOURCE_CREATED', user: result })
-    }
+    result ? handleSuccess(res, result) : handleBadRequest(res, 'User not created.')
   } catch (error) {
-    res.status(500).json({ status: 'ERROR', error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
@@ -60,13 +45,9 @@ const updateById = async (req, res) => {
   const user = req.body.user
   try {
     const result = await User.updateById(id, user)
-    if (!result) {
-      res.status(500).json({ status: 'ERROR', error: 'User not updated' })
-    } else {
-      res.status(201).json({ status: 'RESOURCE_UPDATED', user: result })
-    }
+    result ? handleSuccess(res, result) : handleBadRequest(res, 'User not updated.')
   } catch (error) {
-    res.status(500).json({ status: 'ERROR', error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
@@ -75,13 +56,9 @@ const updateByCedula = async (req, res) => {
   const user = req.body.user
   try {
     const result = await User.updateByCedula(cedula, user)
-    if (!result) {
-      res.status(500).json({ status: 'ERROR', error: 'User not updated' })
-    } else {
-      res.status(201).json({ status: 'RESOURCE_UPDATED', user: result })
-    }
+    result ? handleSuccess(res, result) : handleBadRequest(res, 'User not updated.')
   } catch (error) {
-    res.status(500).json({ status: 'ERROR', error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
@@ -89,13 +66,9 @@ const remove = async (req, res) => {
   const id = req.params.id
   try {
     const result = await User.delete(id)
-    if (!result) {
-      res.status(500).json({ status: 'ERROR', error: 'User not deleted' })
-    } else {
-      res.status(200).json({ status: 'RESOURCE_DELETED', user: result })
-    }
+    result ? handleSuccess(res, result) : handleBadRequest(res, 'User not deleted.')
   } catch (error) {
-    res.status(500).json({ status: 'ERROR', error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
