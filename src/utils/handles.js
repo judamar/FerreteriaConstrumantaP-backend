733cd318ae0458784812024b502b83114b5b5aa9
judamar <juancho.o.mart@gmail.com>
2024-01-12 22:30:58 -0500

    handles for the controllers

---
+++ src/utils/handles.js
@@ -0,0 +1,17 @@
+const handleSuccess = (res, status, data) => {
+  res.status(status).json({ status: 'SUCCESS', data })
+}
+
+const handleNotFound = (res, error) => {
+  res.status(404).json({ status: 'NOT_FOUND', error })
+}
+
+const handleBadRequest = (res, error) => {
+  res.status(400).json({ status: 'BAD_REQUEST', error })
+}
+
+const handleServerError = (res, error) => {
+  res.status(500).json({ status: 'ERROR', error: error.message })
+}
+
+export { handleSuccess, handleNotFound, handleBadRequest, handleServerError }
