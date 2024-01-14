d9cce336e5adb0fc4b993f6564467d560b4044a0
judamar <juancho.o.mart@gmail.com>
2024-01-14 18:00:52 -0500

    coso

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,48 +1,48 @@
 import pool from '../database/db.js'
 
 class Category {
-  static getAll () {
-    return pool.query('SELECT * FROM categorias')
+  static async create (category) {
+    return await pool.query('INSERT INTO categorias (categoria) VALUES (?)', [category])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getById (id) {
-    return pool.query('SELECT * FROM categorias WHERE id = ?', [id])
+  static async getAll () {
+    return await pool.query('SELECT * FROM categorias')
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getByName (name) {
-    return pool.query('SELECT * FROM categorias WHERE categoria = ?', [name])
+  static async getById (id) {
+    return await pool.query('SELECT * FROM categorias WHERE id = ?', [id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static create (category) {
-    return pool.query('INSERT INTO categorias (categoria) VALUES (?)', [category])
+  static async getByName (name) {
+    return await pool.query('SELECT * FROM categorias WHERE categoria = ?', [name])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static update (id, category) {
-    return pool.query('UPDATE categorias SET categoria = ? WHERE id = ?', [category, id])
+  static async update (id, category) {
+    return await pool.query('UPDATE categorias SET categoria = ? WHERE id = ?', [category, id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static remove (id) {
-    return pool.query('DELETE FROM categorias WHERE id = ?', [id])
+  static async remove (id) {
+    return await pool.query('DELETE FROM categorias WHERE id = ?', [id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
