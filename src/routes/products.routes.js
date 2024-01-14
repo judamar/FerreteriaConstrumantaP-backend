329c266a314a3be20b1c5097133bfb814a1d164f
judamar <juancho.o.mart@gmail.com>
2024-01-13 22:36:54 -0500

    add routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,10 +4,12 @@ import Product from '../controllers/products.controller.js'
 const ProductRouter = Router()
 
 ProductRouter
-  .get('/', Product.getAllProducts)
-  .get('/:id', Product.getOneProduct)
-  .post('/', Product.createNewProduct)
-  .put('/:id', Product.updateOneProduct)
-  .delete('/:id', Product.deleteOneProduct)
+  .get('/', Product.getAll)
+  .get('/id/:id', Product.getById)
+  .get('/search/:name', Product.getByName)
+  .get('/category/:category', Product.getByCategory)
+  .post('/', Product.create)
+  .put('/:id', Product.update)
+  .delete('/:id', Product.remove)
 
 export default ProductRouter
