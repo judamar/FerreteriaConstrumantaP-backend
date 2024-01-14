ad8e62ecc46aad14659557fa68ed1f796c2a5b6d
judamar <juancho.o.mart@gmail.com>
2024-01-13 22:47:11 -0500

    improved category model

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -2,63 +2,51 @@ import pool from '../database/db.js'
 
 class Category {
   static getAll () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM categorias')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('SELECT * FROM categorias')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static getById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM categorias WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('SELECT * FROM categorias WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getByName (category) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM categorias WHERE categoria = ?', [category])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static getByName (name) {
+    return pool.query('SELECT * FROM categorias WHERE categoria = ?', [name])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static create (category) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO categorias (categoria) VALUES (?)', [category])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('INSERT INTO categorias (categoria) VALUES (?)', [category])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static update (id, category) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE categorias SET categoria = ? WHERE id = ?', [category, id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('UPDATE categorias SET categoria = ? WHERE id = ?', [category, id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static remove (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM categorias WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('DELETE FROM categorias WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
