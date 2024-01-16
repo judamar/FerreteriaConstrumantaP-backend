a0a86f520abf9eba5031717db4472aa31a501f9b
judamar <juancho.o.mart@gmail.com>
2024-01-15 22:35:34 -0500

    add options for get

--- src/models/provider.model.js
+++ src/models/provider.model.js
@@ -1,54 +1,60 @@
 import pool from '../database/db.js'
 
 class Provider {
-  static getAllProviders () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM proveedores')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static create (provider) {
+    return pool.query('INSERT INTO proveedores (NIT, nombre_proveedor, direccion_proveedor, telefono_proveedor, correo_proveedor, telefono_vendedor) VALUES (?, ?, ?, ?, ?, ?)', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getProviderById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM proveedores WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static getAll () {
+    return pool.query('SELECT * FROM proveedores')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static createProvider (provider) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO proveedores (NIT, nombre_proveedor, direccion_proveedor, telefono_proveedor, correo_proveedor, telefono_vendedor) VALUES (?, ?, ?, ?, ?, ?)', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static getById (id) {
+    return pool.query('SELECT * FROM proveedores WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static updateProvider (id, provider) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE proveedores SET NIT = ?, nombre_proveedor = ?, direccion_proveedor = ?, telefono_proveedor = ?, correo_proveedor = ?, telefono_vendedor = ? WHERE id = ?', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor, id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static getByName (name) {
+    return pool.query('SELECT * FROM proveedores WHERE nombre_proveedor LIKE ?', [`%${name}%`])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static deleteProvider (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM proveedores WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static getByNIT (NIT) {
+    return pool.query('SELECT * FROM proveedores WHERE NIT = ?', [NIT])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
+  }
+
+  static update (id, provider) {
+    return pool.query('UPDATE proveedores SET NIT = ?, nombre_proveedor = ?, direccion_proveedor = ?, telefono_proveedor = ?, correo_proveedor = ?, telefono_vendedor = ? WHERE id = ?', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor, id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
+  }
+
+  static remove (id) {
+    return pool.query('DELETE FROM proveedores WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
