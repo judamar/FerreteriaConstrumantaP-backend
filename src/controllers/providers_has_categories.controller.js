0021003e6af8f219f1b1b39cf7b1f46933441a23
judamar <juancho.o.mart@gmail.com>
2024-01-21 18:58:50 -0500

    fix and improve the controller (feedback)

--- src/controllers/providers_has_categories.controller.js
+++ src/controllers/providers_has_categories.controller.js
@@ -13,53 +13,105 @@ const create = async (req, res) => {
       console.log({ Result: result })
       handleSuccess(res, 201, result)
     } else {
-      console.log(pc.bgRed(''))
+      console.log(pc.bgRed('PROVIDER HAS CATEGORY RELATION NOT CREATED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Relation not created')
     }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(pc.bgRed('CREATING PROVIDER HAS CATEGORY RELATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
 const getAll = async (req, res) => {
   try {
+    console.log(pc.bgGreen('GETTING ALL PROVIDER HAS CATEGORY RELATIONS'))
     const result = await ProviderHasCategory.getAll()
-    res.status(200).json({ status: 'OK', result })
+    if (result && result.length > 0) {
+      console.log(pc.bgGreen('PROVIDER HAS CATEGORY RELATIONS FOUND'))
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('PROVIDER HAS CATEGORY RELATIONS NOT FOUND'))
+      console.log({ Result: result })
+      handleNotFound(res, 'Relations not found')
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(pc.bgRed('GETTING ALL PROVIDER HAS CATEGORY RELATIONS FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
 const getById = async (req, res) => {
   try {
+    console.log(pc.bgGreen('GETTING PROVIDER HAS CATEGORY RELATION BY ID'))
+    console.log({ ID: req.params.id })
     const result = await ProviderHasCategory.getById(req.params.id)
-    res.status(200).json({ status: 'OK', result })
+    if (result && result.length > 0) {
+      console.log(pc.bgGreen('PROVIDER HAS CATEGORY RELATION FOUND'))
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('PROVIDER HAS CATEGORY RELATION NOT FOUND'))
+      console.log({ Result: result })
+      handleNotFound(res, 'Relation not found')
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(pc.bgRed('GETTING PROVIDER HAS CATEGORY RELATION BY ID FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
 const update = async (req, res) => {
+  const provHasCategory = req.body
+  const id = req.params.id
   try {
-    const result = await ProviderHasCategory.update(req.body, req.params.id)
-    res.status(200).json({ status: 'OK', result })
+    console.log(pc.bgGreen('UPDATING PROVIDER HAS CATEGORY RELATION'))
+    console.log({ Relation: provHasCategory, ID: id })
+    const result = await ProviderHasCategory.update(provHasCategory, id)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('PROVIDER HAS CATEGORY RELATION UPDATED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('PROVIDER HAS CATEGORY RELATION NOT UPDATED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Relation not updated')
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(pc.bgRed('UPDATING PROVIDER HAS CATEGORY RELATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
 const remove = async (req, res) => {
+  const id = req.params.id
   try {
-    const result = await ProviderHasCategory.remove(req.params.id)
-    res.status(200).json({ status: 'OK', result })
+    console.log(pc.bgGreen('REMOVING PROVIDER HAS CATEGORY RELATION'))
+    console.log({ ID: id })
+    const result = await ProviderHasCategory.remove(id)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('PROVIDER HAS CATEGORY RELATION REMOVED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('PROVIDER HAS CATEGORY RELATION NOT REMOVED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Relation not removed')
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(pc.bgRed('REMOVING PROVIDER HAS CATEGORY RELATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
 export default {
+  create,
   getAll,
   getById,
-  insert,
   update,
   remove
 }
