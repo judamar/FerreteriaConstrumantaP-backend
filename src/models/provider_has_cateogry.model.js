aa63da14e97fcb5dd78e1217fcd9e38dd6405abe
judamar <juancho.o.mart@gmail.com>
2024-01-21 22:49:12 -0500

    improve providerHasCategory model

--- src/models/provider_has_cateogry.model.js
+++ src/models/provider_has_cateogry.model.js
@@ -2,53 +2,43 @@ import pool from '../database/db.js'
 
 class ProviderHasCategory {
   static create (providerHasCategory) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO proveedores_has_categorias (proveedores_id, categorias_id) VALUES (?, ?)', providerHasCategory.proveedores_id, providerHasCategory.categorias_id)
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('INSERT INTO proveedores_has_categorias (proveedores_id, categorias_id) VALUES (?, ?)', providerHasCategory.proveedores_id, providerHasCategory.categorias_id)
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static getAll () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM proveedores_has_categorias')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('SELECT * FROM proveedores_has_categorias')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static getById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM proveedores_has_categorias WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('SELECT * FROM proveedores_has_categorias WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static update (id, providerHasCategory) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE proveedores_has_categorias SET proveedores_id = ?, categorias_id = ? WHERE id = ?', providerHasCategory.proveedores_id, providerHasCategory.categorias_id, id)
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('UPDATE proveedores_has_categorias SET proveedores_id = ?, categorias_id = ? WHERE id = ?', [providerHasCategory.proveedores_id, providerHasCategory.categorias_id, id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
   static remove (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM proveedores_has_categorias WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+    return pool.query('DELETE FROM proveedores_has_categorias WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
