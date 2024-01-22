2cf6c70675f8bfde7a69c5211d7b2b0a0c71bcf1
judamar <juancho.o.mart@gmail.com>
2024-01-21 23:02:34 -0500

    convert to async await and improve it

--- src/models/reservation.model.js
+++ src/models/reservation.model.js
@@ -1,54 +1,44 @@
 import pool from '../database/db.js'
 
 class Reservation {
-  static getAllReservations () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM reservas')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async create (reservation) {
+    return await pool.query('INSERT INTO reservas (usuario_id, herramienta_maquina_id, fecha_inicio, fecha_fin, cantidad, estado_reserva_id) VALUES (?, ?, ?, ?, ?, ?)', [reservation.usuario_id, reservation.herramienta_maquina_id, reservation.fecha_inicio, reservation.fecha_fin, reservation.cantidad, reservation.estado_reserva_id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getReservationById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM reservas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getAll () {
+    return await pool.query('SELECT * FROM reservas')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static createReservation (reservation) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO reservas(usuario_id, herramienta_maquina_id, fecha_inicio, fecha_fin, cantidad, estado_reserva_id) SET ?, ?, ?, ?, ?, ?', [reservation.usuario_id, reservation.herramienta_maquina_id, reservation.fecha_inicio, reservation.fecha_fin, reservation.cantidad, reservation.estado_reserva_id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getById (id) {
+    return await pool.query('SELECT * FROM reservas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static updateReservation (reservation) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE reservas SET usuario_id = ?, herramienta_maquina_id = ?, fecha_inicio = ?, fecha_fin = ?, cantidad = ?, estado_reserva_id = ? WHERE id = ?', [reservation.usuario_id, reservation.herramienta_maquina_id, reservation.fecha_inicio, reservation.fecha_fin, reservation.cantidad, reservation.estado_reserva_id, reservation.id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async update (id, reservation) {
+    return await pool.query('UPDATE reservas SET usuario_id = ?, herramienta_maquina_id = ?, fecha_inicio = ?, fecha_fin = ?, cantidad = ?, estado_reserva_id = ? WHERE id = ?', [reservation.usuario_id, reservation.herramienta_maquina_id, reservation.fecha_inicio, reservation.fecha_fin, reservation.cantidad, reservation.estado_reserva_id, id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static deleteReservation (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM reservas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async remove (id) {
+    return await pool.query('DELETE FROM reservas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
