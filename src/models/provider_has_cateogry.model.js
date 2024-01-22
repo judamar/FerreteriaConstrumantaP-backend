c6d584859dbe0262ab7207a151acb199d03d6ab9
judamar <juancho.o.mart@gmail.com>
2024-01-21 22:56:33 -0500

    convert to async await

--- src/models/provider_has_cateogry.model.js
+++ src/models/provider_has_cateogry.model.js
@@ -1,40 +1,40 @@
 import pool from '../database/db.js'
 
 class ProviderHasCategory {
-  static create (providerHasCategory) {
-    return pool.query('INSERT INTO proveedores_has_categorias (proveedores_id, categorias_id) VALUES (?, ?)', providerHasCategory.proveedores_id, providerHasCategory.categorias_id)
+  static async create (providerHasCategory) {
+    return await pool.query('INSERT INTO proveedores_has_categorias (proveedores_id, categorias_id) VALUES (?, ?)', providerHasCategory.proveedores_id, providerHasCategory.categorias_id)
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getAll () {
-    return pool.query('SELECT * FROM proveedores_has_categorias')
+  static async getAll () {
+    return await pool.query('SELECT * FROM proveedores_has_categorias')
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getById (id) {
-    return pool.query('SELECT * FROM proveedores_has_categorias WHERE id = ?', [id])
+  static async getById (id) {
+    return await pool.query('SELECT * FROM proveedores_has_categorias WHERE id = ?', [id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static update (id, providerHasCategory) {
-    return pool.query('UPDATE proveedores_has_categorias SET proveedores_id = ?, categorias_id = ? WHERE id = ?', [providerHasCategory.proveedores_id, providerHasCategory.categorias_id, id])
+  static async update (id, providerHasCategory) {
+    return await pool.query('UPDATE proveedores_has_categorias SET proveedores_id = ?, categorias_id = ? WHERE id = ?', [providerHasCategory.proveedores_id, providerHasCategory.categorias_id, id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static remove (id) {
-    return pool.query('DELETE FROM proveedores_has_categorias WHERE id = ?', [id])
+  static async remove (id) {
+    return await pool.query('DELETE FROM proveedores_has_categorias WHERE id = ?', [id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
