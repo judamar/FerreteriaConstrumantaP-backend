3c152d2433efc8712fcdff598f9d47bcda220103
judamar <juancho.o.mart@gmail.com>
2024-01-22 17:24:19 -0500

    improve controller (add feedback and simplify names)

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -1,34 +1,111 @@
 import Reservation from '../models/reservation.model.js'
+import { handleSuccess, handleNotFound, handleServerError, handleBadRequest } from '../utils/handles.js'
+import pc from 'picocolors'
 
-const getAllReservations = async (req, res) => {
-  const result = await Reservation.getAllReservations()
-  res.status(200).json({ status: 'OK', result })
+const create = async (req, res) => {
+  try {
+    console.log(pc.green('CREATING RESERVATION'))
+    const result = await Reservation.create(req.body)
+    if (result) {
+      console.log(pc.green('RESERVATION CREATED'))
+      handleSuccess(res, 201, result)
+    } else {
+      console.log(pc.red('RESERVATION NOT CREATED'))
+      handleBadRequest(res, 'Reservation not created.')
+    }
+  } catch (error) {
+    console.log(pc.red('CREATING RESERVATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
 }
 
-const getReservationById = async (req, res) => {
-  const result = await Reservation.getReservationById(req.params.id)
-  res.status(200).json({ status: 'OK', result })
+const getAll = async (req, res) => {
+  try {
+    console.log(pc.green('GETTING ALL RESERVATIONS'))
+    const result = await Reservation.getAll()
+    if (result && result.length > 0) {
+      console.log(pc.green('RESERVATIONS FOUND'))
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.red('RESERVATIONS NOT FOUND'))
+      console.log({ Result: result })
+      handleNotFound(res, 'Reservations not found.')
+    }
+  } catch (error) {
+    console.log(pc.red('GETTING ALL RESERVATIONS FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
 }
 
-const insertReservation = async (req, res) => {
-  const result = await Reservation.insertReservation(req.body.reserva)
-  res.status(200).json({ status: 'OK', result })
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params
+    console.log(pc.green('GETTING RESERVATION'))
+    const reservation = await Reservation.getById(id)
+    if (reservation && reservation.length > 0) {
+      console.log(pc.green('RESERVATION FOUND'))
+      handleSuccess(res, 200, reservation)
+    } else {
+      console.log(pc.red('RESERVATION NOT FOUND'))
+      console.log({ Reservation: reservation })
+      handleNotFound(res, 'Reservation not found.')
+    }
+  } catch (error) {
+    console.log(pc.red('GETTING RESERVATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
 }
 
-const updateReservation = async (req, res) => {
-  const result = await Reservation.updateReservation(req.body.reserva)
-  res.status(200).json({ status: 'OK', result })
+const update = async (req, res) => {
+  try {
+    const { id } = req.params
+    console.log(pc.bgGreen('UPDATING RESERVATION'))
+    const result = await Reservation.update(id, req.body)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('RESERVATION UPDATED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('RESERVATION NOT UPDATED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Reservation not updated.')
+    }
+  } catch (error) {
+    console.log(pc.bgRed('UPDATING RESERVATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
 }
 
-const deleteReservation = async (req, res) => {
-  const result = await Reservation.deleteReservation(req.params.id)
-  res.status(200).json({ status: 'OK', result })
+const remove = async (req, res) => {
+  const id = req.params.id
+  try {
+    console.log(pc.bgGreen('DELETING RESERVATION'))
+    console.log({ Id: id })
+    const result = await Reservation.remove(id)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('RESERVATION DELETED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('RESERVATION NOT DELETED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Reservation not deleted.')
+    }
+  } catch (error) {
+    console.log(pc.bgRed('DELETING RESERVATION FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
 }
 
 export default {
-  getAllReservations,
-  getReservationById,
-  insertReservation,
-  updateReservation,
-  deleteReservation
+  create,
+  getAll,
+  getById,
+  update,
+  remove
 }
