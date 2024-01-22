69fbea03f084f2a059c999166065959354bb9e2b
judamar <juancho.o.mart@gmail.com>
2024-01-21 23:08:22 -0500

    convert to async await and improve it

--- src/models/sales_detail.model.js
+++ src/models/sales_detail.model.js
@@ -1,54 +1,44 @@
 import pool from '../database/db.js'
 
 class SalesDetail {
-  static getAll () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM detalles_ventas')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async create (saleDetail) {
+    return await pool.query('INSERT INTO detalles_ventas (venta_id, producto_id, cantidad, precio_unitario) VALUES (?, ?, ?, ?)', [saleDetail.venta_id, saleDetail.producto_id, saleDetail.cantidad, saleDetail.precio_unitario])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM detalles_ventas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getAll () {
+    return await pool.query('SELECT * FROM detalles_ventas')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static create (saleDetail) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO detalles_ventas (id_venta, id_producto, cantidad, precio_unitario) VALUES (?, ?, ?, ?)', saleDetail)
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getById (id) {
+    return await pool.query('SELECT * FROM detalles_ventas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static update (saleDetail) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE detalles_ventas SET id_venta = ?, id_producto = ?, cantidad = ?, precio_unitario = ? WHERE id = ?', saleDetail)
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async update (id, saleDetail) {
+    return await pool.query('UPDATE detalles_ventas SET venta_id = ?, producto_id = ?, cantidad = ?, precio_unitario = ? WHERE id = ?', [saleDetail.venta_id, saleDetail.producto_id, saleDetail.cantidad, saleDetail.precio_unitario, id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static delete (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM detalles_ventas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async delete (id) {
+    return await pool.query('DELETE FROM detalles_ventas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
