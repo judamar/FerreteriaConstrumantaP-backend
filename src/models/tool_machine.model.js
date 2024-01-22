27a079dfd845c2a895d1aa8129f9394ce7859f08
judamar <juancho.o.mart@gmail.com>
2024-01-22 16:08:51 -0500

    improve models (async await and reorder)

--- src/models/tool_machine.model.js
+++ src/models/tool_machine.model.js
@@ -1,56 +1,52 @@
 import pool from '../database/db.js'
 
 class ToolMachine {
-  static getAll () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM herramientas_maquinas')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => {
-          reject(err)
-        })
-    })
+  static async create (toolMachine) {
+    return await pool.query('INSERT INTO herramientas_maquinas (nombre_articulo, url_imagen, descripcion, precio_alquiler, cantidad_disponible, estado_herramienta_maquina_id) VALUES (?, ?, ?, ?, ?, ?)', [toolMachine.nombre_articulo, toolMachine.url_imagen, toolMachine.descripcion, toolMachine.precio_alquiler, toolMachine.cantidad_disponible, toolMachine.estado_herramienta_maquina_id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM herramientas_maquinas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getAll () {
+    return await pool.query('SELECT * FROM herramientas_maquinas')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static createToolMachine (toolMachine) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO herramientas_maquinas (nombre_articulo, url_imagen, descripcion, precio_alquiler, cantidad_disponible, estado_herramienta_maquina_id) VALUES (?, ?, ?, ?, ?, ?)', [toolMachine.nombre_articulo, toolMachine.url_imagen, toolMachine.descripcion, toolMachine.precio_alquiler, toolMachine.cantidad_disponible, toolMachine.estado_herramienta_maquina_id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getById (id) {
+    return await pool.query('SELECT * FROM herramientas_maquinas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static updateToolMachine (toolMachine) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE herramientas_maquinas SET nombre_articulo = ?, url_imagen = ?, descripcion = ?, precio_alquiler = ?, cantidad_disponible = ?, estado_herramienta_maquina_id = ? WHERE id = ?', [toolMachine.nombre_articulo, toolMachine.url_imagen, toolMachine.descripcion, toolMachine.precio_alquiler, toolMachine.cantidad_disponible, toolMachine.estado_herramienta_maquina_id, toolMachine.id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getByName (name) {
+    return await pool.query('SELECT * FROM herramientas_maquinas WHERE nombre_articulo LIKE ?', [name])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static deleteToolMachine (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM herramientas_maquinas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async update (toolMachine) {
+    return await pool.query('UPDATE herramientas_maquinas SET nombre_articulo = ?, url_imagen = ?, descripcion = ?, precio_alquiler = ?, cantidad_disponible = ?, estado_herramienta_maquina_id = ? WHERE id = ?', [toolMachine.nombre_articulo, toolMachine.url_imagen, toolMachine.descripcion, toolMachine.precio_alquiler, toolMachine.cantidad_disponible, toolMachine.estado_herramienta_maquina_id, toolMachine.id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
+  }
+
+  static async remove (id) {
+    return await pool.query('DELETE FROM herramientas_maquinas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
