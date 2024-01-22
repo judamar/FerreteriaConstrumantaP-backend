416ed0ea1bf1d5847b5bc768cb1d65b780dea888
judamar <juancho.o.mart@gmail.com>
2024-01-21 22:58:29 -0500

    convert to async await

--- src/models/provider.model.js
+++ src/models/provider.model.js
@@ -1,56 +1,56 @@
 import pool from '../database/db.js'
 
 class Provider {
-  static create (provider) {
-    return pool.query('INSERT INTO proveedores (NIT, nombre_proveedor, direccion_proveedor, telefono_proveedor, correo_proveedor, telefono_vendedor) VALUES (?, ?, ?, ?, ?, ?)', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor])
+  static async create (provider) {
+    return await pool.query('INSERT INTO proveedores (NIT, nombre_proveedor, direccion_proveedor, telefono_proveedor, correo_proveedor, telefono_vendedor) VALUES (?, ?, ?, ?, ?, ?)', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getAll () {
-    return pool.query('SELECT * FROM proveedores')
+  static async getAll () {
+    return await pool.query('SELECT * FROM proveedores')
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getById (id) {
-    return pool.query('SELECT * FROM proveedores WHERE id = ?', [id])
+  static async getById (id) {
+    return await pool.query('SELECT * FROM proveedores WHERE id = ?', [id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getByName (name) {
-    return pool.query('SELECT * FROM proveedores WHERE nombre_proveedor LIKE ?', [`%${name}%`])
+  static async getByName (name) {
+    return await pool.query('SELECT * FROM proveedores WHERE nombre_proveedor LIKE ?', [`%${name}%`])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static getByNIT (NIT) {
-    return pool.query('SELECT * FROM proveedores WHERE NIT = ?', [NIT])
+  static async getByNIT (NIT) {
+    return await pool.query('SELECT * FROM proveedores WHERE NIT = ?', [NIT])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static update (id, provider) {
-    return pool.query('UPDATE proveedores SET NIT = ?, nombre_proveedor = ?, direccion_proveedor = ?, telefono_proveedor = ?, correo_proveedor = ?, telefono_vendedor = ? WHERE id = ?', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor, id])
+  static async update (id, provider) {
+    return await pool.query('UPDATE proveedores SET NIT = ?, nombre_proveedor = ?, direccion_proveedor = ?, telefono_proveedor = ?, correo_proveedor = ?, telefono_vendedor = ? WHERE id = ?', [provider.NIT, provider.nombre_proveedor, provider.direccion_proveedor, provider.telefono_proveedor, provider.correo_proveedor, provider.telefono_vendedor, id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
       })
   }
 
-  static remove (id) {
-    return pool.query('DELETE FROM proveedores WHERE id = ?', [id])
+  static async remove (id) {
+    return await pool.query('DELETE FROM proveedores WHERE id = ?', [id])
       .then(([rows, fields]) => rows)
       .catch(err => {
         throw err
