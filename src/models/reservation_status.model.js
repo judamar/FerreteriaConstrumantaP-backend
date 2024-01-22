f49ce701a6f5ce8deb3886defdc272d172495a6f
judamar <juancho.o.mart@gmail.com>
2024-01-21 22:55:06 -0500

    convert to async await and improve it

--- src/models/reservation_status.model.js
+++ src/models/reservation_status.model.js
@@ -1,54 +1,44 @@
 import pool from '../database/db.js'
 
 class ReservationStatus {
-  static create (status) {
-    return new Promise((resolve, reject) => {
-      pool.query('INSERT INTO estados_reservas (estado) VALUES (?)', [status])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async create (status) {
+    return await pool.query('INSERT INTO estados_reservas (estado) VALUES (?)', [status])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getAll () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM estados_reservas')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getAll () {
+    return await pool.query('SELECT * FROM estados_reservas')
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static getById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM estados_reservas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async getById (id) {
+    return await pool.query('SELECT * FROM estados_reservas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static update (id, status) {
-    return new Promise((resolve, reject) => {
-      pool.query('UPDATE estados_reservas SET estado = ? WHERE id = ?', [status, id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async update (id, status) {
+    return await pool.query('UPDATE estados_reservas SET estado = ? WHERE id = ?', [status, id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 
-  static remove (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('DELETE FROM estados_reservas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
+  static async remove (id) {
+    return await pool.query('DELETE FROM estados_reservas WHERE id = ?', [id])
+      .then(([rows, fields]) => rows)
+      .catch(err => {
+        throw err
+      })
   }
 }
 
