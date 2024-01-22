d0b3811c5774f460370cc80000b6acf7c1c438c9
judamar <juancho.o.mart@gmail.com>
2024-01-21 22:16:27 -0500

    re order methods

--- src/models/reservation_status.model.js
+++ src/models/reservation_status.model.js
@@ -1,27 +1,7 @@
 import pool from '../database/db.js'
 
 class ReservationStatus {
-  static getAllReservationStatus () {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM estados_reservas')
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
-  }
-
-  static getReservationStatusById (id) {
-    return new Promise((resolve, reject) => {
-      pool.query('SELECT * FROM estados_reservas WHERE id = ?', [id])
-        .then(([rows, fields]) => {
-          resolve(rows)
-        })
-        .catch(err => reject(err))
-    })
-  }
-
-  static createReservationStatus (status) {
+  static create (status) {
     return new Promise((resolve, reject) => {
       pool.query('INSERT INTO estados_reservas (estado) VALUES (?)', [status])
         .then(([rows, fields]) => {
@@ -31,7 +11,27 @@ class ReservationStatus {
     })
   }
 
-  static updateReservationStatus (id, status) {
+  static getAll () {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM estados_reservas')
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(err => reject(err))
+    })
+  }
+
+  static getById (id) {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM estados_reservas WHERE id = ?', [id])
+        .then(([rows, fields]) => {
+          resolve(rows)
+        })
+        .catch(err => reject(err))
+    })
+  }
+
+  static update (id, status) {
     return new Promise((resolve, reject) => {
       pool.query('UPDATE estados_reservas SET estado = ? WHERE id = ?', [status, id])
         .then(([rows, fields]) => {
@@ -41,7 +41,7 @@ class ReservationStatus {
     })
   }
 
-  static deleteReservationStatus (id) {
+  static remove (id) {
     return new Promise((resolve, reject) => {
       pool.query('DELETE FROM estados_reservas WHERE id = ?', [id])
         .then(([rows, fields]) => {
