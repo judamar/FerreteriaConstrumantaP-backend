e264f05d492d730716bcac89cbb38284370ff9da
judamar <juancho.o.mart@gmail.com>
2024-01-21 22:43:24 -0500

    complete reservation statuses

--- src/controllers/reservation_statuses.controller.js
+++ src/controllers/reservation_statuses.controller.js
@@ -1,12 +1,119 @@
 import ReservationStatus from '../models/reservation_status.model.js'
+import { handleSuccess, handleNotFound, handleServerError, handleBadRequest } from '../utils/handles.js'
+import pc from 'picocolors'
 
-const getAllReservationStatuses = async (req, res) => {
+const create = async (req, res) => {
+  const reservationStatus = req.body
   try {
-    const result = await ReservationStatus.getAllReservationStatus()
-    res.json({ status: 'OK', reservation_status: result })
+    console.log(pc.bgGreen('CREATING RESERVATION STATUS'))
+    console.log({ ReservationStatus: reservationStatus })
+    const result = await ReservationStatus.create(reservationStatus)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('RESERVATION STATUS CREATED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 201, result)
+    } else {
+      console.log(pc.bgRed('RESERVATION STATUS NOT CREATED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Reservation status not created.')
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    console.log(pc.bgRed('CREATING RESERVATION STATUS FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
   }
 }
 
-export default { getAllReservationStatuses }
+const getAll = async (req, res) => {
+  try {
+    console.log(pc.bgGreen('GETTING RESERVATION STATUSES'))
+    const reservationStatuses = await ReservationStatus.getAll()
+    if (reservationStatuses && reservationStatuses.length > 0) {
+      console.log(pc.bgGreen('RESERVATION STATUSES FOUND'))
+      handleSuccess(res, 200, reservationStatuses)
+    } else {
+      console.log(pc.bgRed('RESERVATION STATUSES NOT FOUND'))
+      console.log({ ReservationStatuses: reservationStatuses })
+      handleNotFound(res, 'Reservation statuses not found.')
+    }
+  } catch (error) {
+    console.log(pc.bgRed('GETTING RESERVATION STATUSES FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
+}
+
+const getById = async (req, res) => {
+  const id = req.params.id
+  try {
+    console.log(pc.bgGreen('GETTING RESERVATION STATUS'))
+    console.log({ Id: id })
+    const reservationStatus = await ReservationStatus.getById(id)
+    if (reservationStatus && reservationStatus.length > 0) {
+      console.log(pc.bgGreen('RESERVATION STATUS FOUND'))
+      handleSuccess(res, 200, reservationStatus)
+    } else {
+      console.log(pc.bgRed('RESERVATION STATUS NOT FOUND'))
+      console.log({ ReservationStatus: reservationStatus })
+      handleNotFound(res, 'Reservation status not found.')
+    }
+  } catch (error) {
+    console.log(pc.bgRed('GETTING RESERVATION STATUS FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
+}
+
+const update = async (req, res) => {
+  const id = req.params.id
+  const reservationStatus = req.body
+  try {
+    console.log(pc.bgGreen('UPDATING RESERVATION STATUS'))
+    console.log({ ReservationStatus: reservationStatus })
+    console.log({ Id: id })
+    const result = await ReservationStatus.update(id, reservationStatus)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('RESERVATION STATUS UPDATED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('RESERVATION STATUS NOT UPDATED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Reservation status not updated.')
+    }
+  } catch (error) {
+    console.log(pc.bgRed('UPDATING RESERVATION STATUS FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
+}
+
+const remove = async (req, res) => {
+  const id = req.params.id
+  try {
+    console.log(pc.bgGreen('DELETING RESERVATION STATUS'))
+    console.log({ Id: id })
+    const result = await ReservationStatus.remove(id)
+    if (result && result.affectedRows > 0) {
+      console.log(pc.bgGreen('RESERVATION STATUS DELETED SUCCESFULLY'))
+      console.log({ Result: result })
+      handleSuccess(res, 200, result)
+    } else {
+      console.log(pc.bgRed('RESERVATION STATUS NOT DELETED'))
+      console.log({ Result: result })
+      handleBadRequest(res, 'Reservation status not deleted.')
+    }
+  } catch (error) {
+    console.log(pc.bgRed('DELETING RESERVATION STATUS FAILED'))
+    console.error({ Error: error.message })
+    handleServerError(res, error.message)
+  }
+}
+
+export default {
+  create,
+  getAll,
+  getById,
+  update,
+  remove
+}
--- src/routes/reservation_statuses.routes.js
+++ src/routes/reservation_statuses.routes.js
@@ -4,6 +4,10 @@ import ReservationStatus from '../controllers/reservation_statuses.controller.js
 const ReservationStatusRouter = Router()
 
 ReservationStatusRouter
-  .get('/', ReservationStatus.getAllReservationStatuses)
+  .post('/', ReservationStatus.create)
+  .get('/', ReservationStatus.getAll)
+  .get('/:id', ReservationStatus.getById)
+  .put('/:id', ReservationStatus.update)
+  .delete('/:id', ReservationStatus.remove)
 
 export default ReservationStatusRouter
