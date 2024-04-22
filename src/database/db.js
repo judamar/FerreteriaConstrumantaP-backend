6fbc51d216096a2ce0a803f0ae17d5bb5f4f1b5f
judamar <juancho.o.mart@gmail.com>
2024-04-21 23:06:51 -0500

    using localhost as db

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 import { createPool } from 'mysql2/promise'
 import 'dotenv/config'
-import fs from 'fs'
+// import fs from 'fs'
 
 /* The code is creating a connection pool using the `createPool` function from the `mysql2/promise`
 library. */
@@ -8,8 +8,8 @@ const pool = createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE,
-  ssl: fs.readFileSync(process.env.SSL_KEY)
+  database: process.env.DB_DATABASE || ''
+  // ssl: fs.readFileSync(process.env.SSL_KEY)
 })
 
 export default pool
